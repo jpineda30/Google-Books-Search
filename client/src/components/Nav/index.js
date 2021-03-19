bb89bbfae753e63d579e324692a77c8140ddc745
pepe30 <jose.pinedaesquer@gmail.com>
2021-03-19 12:41:45 -0600

    rearranged files

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+class Nav extends Component {
+  state = {
+    open: false,
+    width: window.innerWidth
+  };
+
+  updateWidth = () => {
+    const newState = { width: window.innerWidth };
+
+    if (this.state.open && newState.width > 991) {
+      newState.open = false;
+    }
+
+    this.setState(newState);
+  };
+
+  toggleNav = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWidth);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWidth);
+  }
+
+  render() {
+    return (
+      <nav className="mainMenu flex-row flex-wrap white">
+
+          <div className="pt-2">
+               <Link className="title" to="/">
+                  Google Books
+              </Link>
+            </div>
+
+
+            <div className="menu-button ">
+              <Link
+                onClick={this.toggleNav}
+                className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
+                to="/"
+              >
+                Search
+              </Link>
+            </div>
+            <div className="menu-button">
+              <Link
+                onClick={this.toggleNav}
+                className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+                to="/saved"
+              >
+                Saved
+              </Link>
+            </div>
+        
+        
+      </nav>
+    );
+  }
+}
+
+export default Nav;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Form from "../components/Form";
+import Book from "../components/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+
+class Home extends Component {
+  state = {
+    books: [],
+    q: "",
+    message: "Search For A Book To Begin!"
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  getBooks = () => {
+    API.getBooks(this.state.q)
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
+      .catch(() =>
+        this.setState({
+          books: [],
+          message: "No New Books Found, Try a Different Query"
+        })
+      );
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.getBooks();
+  };
+
+  handleBookSave = id => {
+    const book = this.state.books.find(book => book.id === id);
+
+    API.saveBook({
+      googleId: book.id,
+      title: book.volumeInfo.title,
+      subtitle: book.volumeInfo.subtitle,
+      link: book.volumeInfo.infoLink,
+      authors: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      image: book.volumeInfo.imageLinks.thumbnail
+    }).then(() => this.getBooks());
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Row>
+          <Col size="md-12">
+            <div className="flex-col flex-center jumbo">
+              <h1 className="text-center">
+                <strong>(React) Google Books Search</strong>
+              </h1>
+              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+            </div>
+          </Col>
+          <Col size="md-12">
+           </Col>
+        </Row>
+        <div className="flex-row">
+
+      
+          <div className="search" title="Book Search" icon="far fa-book">
+                <Form
+                  handleInputChange={this.handleInputChange}
+                  handleFormSubmit={this.handleFormSubmit}
+                  q={this.state.q}
+                />
+              </div>
+         
+
+          
+            <div className="results white" title="Results">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <Book
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      subtitle={book.volumeInfo.subtitle}
+                      link={book.volumeInfo.infoLink}
+                      authors={book.volumeInfo.authors.join(", ")}
+                      description={book.volumeInfo.description}
+                      image={book.volumeInfo.imageLinks.thumbnail}
+                      Button={() => (
+                        <button
+                          onClick={() => this.handleBookSave(book.id)}
+                          className="btn btn-primary ml-2"
+                        >
+                          Save
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">{this.state.message}</h2>
+              )}
+            </div>
+         
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
